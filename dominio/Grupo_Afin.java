9053890022bd283fe2a0fa13bc1b4f556aa49e44
miguel.angel.aranda <miguel.angel.aranda@fib.upc.edu>
2015-04-02 16:21:52 +0200

    Update GrupoAfin

--- dominio/Grupo_Afin.java
+++ dominio/Grupo_Afin.java
@@ -1,5 +1,46 @@
 package dominio;
 
-public class Grupo_Afin {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
+public class Grupo_Afin {
+	private Integer Identificador;
+	private Set<String> Diputados;
+	private Map<String, Resultado_de_busqueda> Conjunto_resultados;
+	
+	public Grupo_Afin(Integer ID) {
+		Identificador = ID;
+		Diputados = new TreeSet<String>();
+		Conjunto_resultados = new TreeMap<String, Resultado_de_busqueda>();
+	}
+	
+	public Boolean addDiputado(String Diputado) {
+		return Diputados.add(Diputado);
+	}
+	
+	public Integer getID() {
+		return Identificador;
+	}
+	
+	public List<String> getDiputados() {
+		List<String> Lista_Diputados = new ArrayList<String>();
+		Iterator<String> it = Diputados.iterator();
+		while (it.hasNext())
+			Lista_Diputados.add(it.next());
+		return Lista_Diputados;
+	}
+	
+	public Boolean pertenece(String Diputado) {
+		return Diputados.contains(Diputado);
+	}
+	
+	public Boolean removeDiputado(String Diputado) {
+		return Diputados.remove(Diputado);
+	}
+	
 }
