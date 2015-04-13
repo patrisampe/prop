84c002d351a9a7d68e609958eb8ea31ef6e235e9
yoel.cabo <yoel.cabo@est.fib.upc.edu>
2015-04-13 20:33:45 +0200

    comencem GrafLouvain

--- dominio/algoritmos/GrafLouvain.java
+++ dominio/algoritmos/GrafLouvain.java
@@ -1,5 +1,8 @@
 package dominio.algoritmos;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class GrafLouvain extends Graf {
 	
 	private Double sumaFila(Integer i) {
@@ -23,8 +26,57 @@ public class GrafLouvain extends Graf {
 		for (Integer i = 0; i < Matriu.size(); ++i) {
 			sum += sumaFila(i);
 		}
+		if (Matriu.size() == 0) return -1.0;
 		return sum/2;
 	}
+	
+	public Double sumaPesos(HashSet<String> Comunitat) {
+		if (Comunitat.isEmpty()) return -1.0;
+		Double sum = 0.0;
+		Iterator<String> it = Comunitat.iterator();
+		while(it.hasNext()) {
+			Double sumi = sumaPesosAdjacents(it.next(), Comunitat);
+			if (sumi > 0) sum += sumi;
+		}
+		return sum;
+	}
+	
+	public Double sumaPesosAdjacentsInclusiva(HashSet<String> Comunitat) {
+		if (Comunitat.isEmpty()) return -1.0;
+		Double sum = 0.0;
+		Iterator<String> it = Comunitat.iterator();
+		while(it.hasNext()) {
+			Double sumi = sumaPesosAdjacents(it.next());
+			if (sumi > 0) sum += sumi;
+		}
+		return sum;
+	}
+	
+	public Double sumaPesosAdjacents(String id) {
+		if (!existeixNode(id)) return -1.0;
+		Integer Posicio = Diccionari.get(id);
+		return sumaFila(Posicio);
+	}
+	
+	public Double sumaPesosAdjacents(String id,HashSet<String> Comunitat) {//Incompletee
+		Integer Posicio = Diccionari.get(id);
+		
+		return sumaFila(Posicio);
+	}
+	
+	public Double sumaPesosAdjacents(HashSet<String> C1, HashSet<String> C2) {
+		if (C1.isEmpty() || C2.isEmpty()) return -1.0;
+		Double sum = 0.0;
+		Iterator<String> it = C1.iterator();
+		while(it.hasNext()) {
+			Double sumi = sumaPesosAdjacents(it.next(), C2);
+			if (sumi > 0) sum += sumi;
+		}
+		return sum;
+	}
+	
+	
+	
 	
 	
 }
