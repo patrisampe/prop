14d2f4909b46d0a38a2087a198ea1579e34ab0e8
patricia.sampedro <patricia.sampedro@est.fib.upc.edu>
2015-05-01 11:10:18 +0200

    versio1

--- controladores/ControladorDominioEvento.java
+++ controladores/ControladorDominioEvento.java
@@ -1,123 +1,80 @@
 package controladores;
 
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
-import java.util.Iterator;
+import java.util.Set;
 
-import utiles.Atributos_Evento;
-import utiles.Atributos_TipoEvento;
 import utiles.CodiError;
-import dominio.Tipo_de_Evento;
-import dominio.Evento;
+import utiles.Conjunto;
+import dominio.TipoEvento;
+import time.Date;
 
 
 public class ControladorDominioEvento {
+	private Conjunto<TipoEvento> conjuntoTipoEvento;
+	private static ControladorDominioEvento instance = null;
+	private CodiError error;
+	private Boolean hasError;
 
-	private Map<String, Tipo_de_Evento> Conjunto_tipos;
-		
-	public ControladorDominioEvento(){
-		Conjunto_tipos = new TreeMap<String, Tipo_de_Evento>();
-	}
 	
-	public CodiError SetAtributosTipoEvento(String nombreTipoEvento, Atributos_TipoEvento atributos){
-		Tipo_de_Evento original = Conjunto_tipos.get(nombreTipoEvento);
-		Tipo_de_Evento nuevo= new Tipo_de_Evento(nombreTipoEvento,original);
-		
-		Integer nuevaImportancia=atributos.getImportancia();
-		
-		if(!Tipo_de_Evento.es_validaImportancia(nuevaImportancia))return new CodiError(9,nombreTipoEvento);
-		nuevo.SetImportancia(nuevaImportancia);
-		
-		String[] aux2;
-		aux2 = atributos.getEventos();
-		
-		
-		for(int i=0; i<aux2.length;++i){
-			if(!nuevo.Es_Evento(aux2[i])) return new CodiError(7,aux2[i]);
-			nuevo.RemoveEvento(aux2[i]);
-		}
-		Conjunto_tipos.put(nombreTipoEvento, nuevo);
-		return new CodiError(0,nombreTipoEvento);
-	}
-	
-	public Atributos_TipoEvento GetAtributosTipoEvento(String nombreTipoEvento){
-		
-		return new Atributos_TipoEvento(Conjunto_tipos.get(nombreTipoEvento).GetImportancia(),Atributos_TipoEvento.EventosUnchanged);
-	}
-	
-	public List<String> GetTipoEventos(){
-		return new ArrayList<String>(Conjunto_tipos.keySet());
-	}
-	
-	public CodiError AddTipoEvento(String nombreTipoEvento, Integer importancia){
-		
-		if(Conjunto_tipos.containsKey(nombreTipoEvento))return new CodiError(12,nombreTipoEvento);
-		Tipo_de_Evento aux=new Tipo_de_Evento(nombreTipoEvento,importancia);
-		Conjunto_tipos.put(nombreTipoEvento, aux);
-		return new CodiError(0,nombreTipoEvento);
-	}
-	
-	public CodiError RemoveTipoEvento(String nombreTipoEvento){
-		if(!Conjunto_tipos.containsKey(nombreTipoEvento))return new CodiError(13,nombreTipoEvento);
-		Conjunto_tipos.remove(nombreTipoEvento);
-		return new CodiError(0,nombreTipoEvento);
-	}
-	
+	   protected ControladorDominioEvento() {
+	      // Exists only to defeat instantiation.
+		   conjuntoTipoEvento=  new Conjunto<TipoEvento>(TipoEvento.class);
+	   }
+	   
+	   public static ControladorDominioEvento getInstance() {
+	      if(instance == null) {
+	         instance = new ControladorDominioEvento();  
+	      }
+	      
+	      return instance;
+	   }
+	   
+	   
+	   public void setImportanciaTipoEvento(String nombreTipoEvento, Integer Importancia){
+		   
+	   }
+	   
+	   public Integer getImportanciaTipoEvento(String nombreTipoEvento){
+		  return conjuntoTipoEvento.get(nombreTipoEvento).getImportancia();
+	   }
+	   
+	   public Set<String> getTipoEvento(){
+		   return conjuntoTipoEvento.getStringKeys();
+	   }
+	   
+	   public List<String> getEventos(String nombreTipoEvento){
+		   return conjuntoTipoEvento.get(nombreTipoEvento).getEventos();
+		   
+	   }
+	   
+	   public List<String> getEventos(String nombreTipoEvento, Date dataInici, Date dataFi){
+		   List<String> a=new ArrayList<String>();
+		   
+		   
+		   
+		   return a;
+		   
+	   }
+	   
+	   /*
 
-	public Boolean Es_TipoEvento(String nombreTipoEvento){
-		return Conjunto_tipos.containsKey(nombreTipoEvento);
-	}
-	
-	
-	public List<String> GetEventos(String nombreTipoEvento){
-		return Conjunto_tipos.get(nombreTipoEvento).GetEventos();
-	}
-	
-	
-	public CodiError AddEvento(String nombreTipoEvento, String nombreEvento, Atributos_Evento atributos){
-		Tipo_de_Evento tp= Conjunto_tipos.get(nombreTipoEvento);
-		if(tp.Es_Evento(nombreEvento))return new CodiError(8,nombreEvento);
-		if(!atributos.getFecha().Es_valida()) return new CodiError(2,nombreEvento);
-		Evento event = new Evento(nombreEvento,atributos.getFecha());
-		tp.AddEvento(event);
-		return new CodiError(0,nombreTipoEvento);
-	}
+	+ setImportanciaTipoEvento(String nombreTipoEvento, Integer Importancia): void
+	+ addTipoEvento(String nombreTipoEvento, Integer importancia): void
+	+ removeTipoEvento(String nombreTipoEvento): void
+	+ esTipoEvento(String nombreTipoEvento): Boolean
+	+ getEventos(String nombreTipoEvento, Date dataInici, Date dataFi): List<String>
 
-	public CodiError SetAtributosEvento(String nombreTipoEvento, String nombreEvento, Atributos_Evento atributos){
-		
-		
-		if(!atributos.getFecha().Es_valida())return new CodiError(2,nombreEvento);
-		Tipo_de_Evento original= Conjunto_tipos.get(nombreTipoEvento);
-		Evento eoriginal=original.GetEvento(nombreEvento);
-		Evento enuevo= new Evento(eoriginal.GetNombre(),eoriginal);
-		enuevo.SetFecha(atributos.getFecha());
-		
-		Map<String,Boolean> dipu= atributos.getDiputados();
-		Iterator<Entry<String, Boolean>> iterator = dipu.entrySet().iterator();
-		while (iterator.hasNext()) {
-			Entry<String, Boolean> mapEntry = dipu.entrySet().iterator().next();
-			if(mapEntry.getValue())
-			{
-				//insertamos evento
-				if(enuevo.Es_participante(mapEntry.getKey()))return new CodiError(15,mapEntry.getKey());
-				enuevo.AddDiputado(mapEntry.getKey());
-			}
-			else
-			{
-				//borramos evento
-				if(!enuevo.Es_participante(mapEntry.getKey()))return new CodiError(14,mapEntry.getKey());
-				enuevo.RemoveDiputado(mapEntry.getKey());
-			}
-		}
-		original.AddEvento(enuevo);//remplazamos el evento que ya habia
-		return new CodiError(0,nombreTipoEvento);
-	}
+	+ addEvento(String nombreTipoEvento, String nombreEvento, Date fecha, Set<String> Diputados):void
+	+ removeEvento(String nombreTipoEvento, String nombreEvento):void
+	+ esEvento(String nombreTipoEvento, String nombreEvento):Boolean
+	+ setFechaEvento(String nombreTipoEvento, String nombreEvento, Date fecha): void
+	+ getFechaEvento(String nombreTipoEvento, String nombreEvento): Date
+	+ getDiputadosEvento(String nombreTipoEvento, String nombreEvento): Set<String>
+	+ addDiputadoEvento(String nombreTipoEvento, String nombreEvento, String nombreDiputado):void
+	+ removeDiputadoEvento(String nombreTipoEvento, String nombreEvento, String nombreDiputado):void
+	+ removeDiputado(String nombreDIputado):void
 
-	public Atributos_Evento GetAtributosEvento(String nombreTipoEvento, String nombreEvento){
-		return new Atributos_Evento(Conjunto_tipos.get(nombreTipoEvento).GetEvento(nombreEvento).GetFecha(),Atributos_Evento.DiputadosUnchanged);
-	}
+*/
 	
 }
