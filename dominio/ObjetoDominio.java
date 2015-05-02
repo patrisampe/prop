fdd9d02cb2bda37d8ebbd292ee98a8ab38b2b342
David Moran <david.moran@est.fib.upc.edu>
2015-05-02 20:28:23 +0200

    ObjetoDominio

--- dominio/ObjetoDominio.java
+++ dominio/ObjetoDominio.java
@@ -1,17 +1,31 @@
 package dominio;
 
+/**
+ * Objeto generico del dominio de datos.
+ * @author David Moran
+ */
 public class ObjetoDominio {
 	
-	public ObjetoDominio(){
-		
+	/**
+	 * Crea una instancia de un objeto generico del dominio.
+	 */
+	protected ObjetoDominio(){
 	}
 	
+	/**
+	 * Consulta el nombre generico de un objeto del dominio.
+	 * @return Un <i>String</i> vacio ("").
+	 */
 	public String getNombre(){
 		return "";
 	}
 	
+	/**
+	 * Consulta el identificador generico de un objeto del dominio.
+	 * @return Un <i>Integer</i> con valor -2.
+	 */
 	public Integer getID(){
-		return -1;
+		return -2;
 	}
 	
 }
