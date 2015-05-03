877879bfbf04e5ecd82303b3402b13423b62d3fb
miguel.angel.aranda <miguel.angel.aranda@fib.upc.edu>
2015-05-03 03:15:44 +0200

    e

--- dominio/GrupoAfinPorPeriodo.java
+++ dominio/GrupoAfinPorPeriodo.java
@@ -1,7 +1,15 @@
 package dominio;
 
+/**
+ * Grupo afin obtenido en una busqueda por periodo.
+ * @author Miguel Angel Aranda
+ * @version 1.0 Mayo 2015
+ */
 public class GrupoAfinPorPeriodo extends GrupoAfin {
 	
+	/**
+	 * Crea una nueva instancia de grupo afin por periodo.
+	 */
 	public GrupoAfinPorPeriodo(Integer ID) {
 		super(ID);
 	}
