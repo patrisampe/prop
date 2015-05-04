d82024308565c144e90786f18395a69e5598094d
miguel.angel.aranda <miguel.angel.aranda@fib.upc.edu>
2015-05-04 09:07:55 +0200

    Por fin

--- dominio/ResultadoDeBusqueda.java
+++ dominio/ResultadoDeBusqueda.java
@@ -2,11 +2,7 @@ package dominio;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
-
-import utiles.Conjunto;
 
 /**
  * Resultado obtenido por la busqueda de grupos afines entre diputados (Clase abstracta).
@@ -41,11 +37,6 @@ public abstract class ResultadoDeBusqueda extends ObjetoDominio{
 	private Boolean modificado;
 	
 	/**
-	 * Conjunto de grupos afines.
-	 */
-	protected Conjunto<GrupoAfin> gruposAfines;
-	
-	/**
 	 * Constructor de la clase resultado de busqueda.
 	 * @param nombre - Nombre del resultado.
 	 * @param indiceAfinidad - Indice de afinidad utilizado para obtener el resultado.
@@ -76,74 +67,6 @@ public abstract class ResultadoDeBusqueda extends ObjetoDominio{
 	 */
 	public void addImportancia(String nombreTipoEvento, Integer importancia) {
 		this.importancia.put(nombreTipoEvento,importancia);
-	}
-	
-	/**
-	 * Elimina un diputado de todos los grupos afines donde se encuentre.
-	 * @param nombre - Nombre del diputado a eliminar.
-	 */
-	public void removeDiputado(String nombre) {
-		for (GrupoAfin grup:gruposAfines.getAll()) {
-			grup.removeDiputado(nombre);
-			if (grup.esVacio()) eliminarGrupo(grup.getID());
-		}
-	}
-	
-	/**
-	 * Agrega un diputado a un grupo afin en concreto.
-	 * @param nombre - Nombre del diputado a agregar.
-	 * @param ID - Identificador del grupo al que es agregado.
-	 */
-	public void addDiputado(String nombre, Integer ID) {
-		gruposAfines.get(ID).addDiputado(nombre);
-	}
-	
-	/**
-	 * Elimina un diputado de un grupo afin en concreto.
-	 * @param nombre - Nombre del diputado a eliminar.
-	 * @param ID - Identificador del grupo del que es eliminado.
-	 */
-	public void removeDiputado(String nombre, Integer ID) {
-		gruposAfines.get(ID).removeDiputado(nombre);
-		if (gruposAfines.get(ID).esVacio())
-			gruposAfines.remove(ID);
-	}
-
-	/**
-	 * Mueve un diputado de un grupo afin a otro.
-	 * @param nombreDiputado - Diputado que se debe mover.
-	 * @param desdeID - Identificador del grupo afin del que se extrae el diputado.
-	 * @param hastaID - Identificador del grupo afin al que se agrega el diputado.
-	 */
-	public void moveDiputado(String nombre, Integer desdeID, Integer hastaID) {
-		addDiputado(nombre, hastaID);
-		removeDiputado(nombre, desdeID);
-	}
-	
-	/**
-	 * Añade un nuevo grupo al conjunto de grupos afines.
-	 */
-	public void addGrupo(GrupoAfin nuevoGrupo) {
-		gruposAfines.add(nuevoGrupo.getID(), nuevoGrupo);
-	}
-	
-	/**
-	 * Elimina un grupo del resultado.
-	 * @param ID - Identificador del grupo del que es eliminado.
-	 */
-	public void eliminarGrupo(Integer ID) {
-		gruposAfines.remove(ID);
-	}
-
-	/**
-	 * Comprueba si existe un grupo en concreto.
-	 * @param ID - Identificador del grupo que se desea comprobar.
-	 * @return <i>true</i> si el grupo existe en el conjunto de grupos afines.
-	 * <br>
-	 * <i>false</i> en cualquier otro caso..
-	 */
-	public Boolean existeGrupo(Integer ID) {
-		return gruposAfines.exists(ID);
 	}
 	
 	/**
@@ -212,25 +135,6 @@ public abstract class ResultadoDeBusqueda extends ObjetoDominio{
 	 */
 	public String getPeriodo() {
 		return "";
-	}
-	
-	/**
-	 * Suministra un nuevo conjunto con todos los grupos afines del resultado.
-	 * @return Conjunto de grupos afines.
-	 */
-	public Conjunto<GrupoAfin> getGruposAfines() {
-		return new Conjunto<GrupoAfin>(this.gruposAfines);
-	}
-	
-	/**
-	 * Suministra un conjunto de cadenas de texto con todos los nommbres de los resultados.
-	 * @return El periodo utilizado en la busqueda.
-	 */
-	public Set<Set<String>> getResultado() {
-		Set<Set<String>> listaResultado = new TreeSet<Set<String>>();
-		for (GrupoAfin grup:gruposAfines.getAll())
-			listaResultado.add(grup.getDiputados());
-		return listaResultado;
 	}
 	
 	/**
--- dominio/ResultadoDeBusquedaPorDiputado.java
+++ dominio/ResultadoDeBusquedaPorDiputado.java
@@ -1,6 +1,9 @@
 package dominio;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
 import utiles.Conjunto;
 
 /**
@@ -21,6 +24,11 @@ public class ResultadoDeBusquedaPorDiputado extends ResultadoDeBusqueda {
 	private Integer lapsoDeTiempo;
 	
 	/**
+	 * Conjunto de grupos afines.
+	 */
+	private Conjunto<GrupoAfinPorDiputado> gruposAfines;
+	
+	/**
 	 * Crea una instancia de la clase resultado de busqueda por diputado.
 	 * @param nombre - Nombre del resultado.
 	 * @param indiceAfinidad - Indice de afinidad utilizado para obtener el resultado.
@@ -33,12 +41,100 @@ public class ResultadoDeBusquedaPorDiputado extends ResultadoDeBusqueda {
 	 */
 	public ResultadoDeBusquedaPorDiputado(String nombre, Integer indiceAfinidad, TipoAlgoritmo algoritmo, Map<String, Integer> importancia, Boolean modificado, Integer lapsoDeTiempo, Conjunto<GrupoAfinPorDiputado> gruposAfines, String diputadoRelevante) {
 			super(nombre, indiceAfinidad, algoritmo, importancia, modificado);
-			this.gruposAfines = new Conjunto<GrupoAfin>(GrupoAfin.class);
+			this.gruposAfines = new Conjunto<GrupoAfinPorDiputado>(GrupoAfinPorDiputado.class);
 			for (GrupoAfinPorDiputado grup:gruposAfines.getAll()) {
 				this.gruposAfines.add(grup.getID(), grup);
-				if (existeGrupo(grup.getID())) System.out.println("Dishhhh");
 			}
+			this.lapsoDeTiempo = lapsoDeTiempo;
 			this.diputadoRelevante = diputadoRelevante;
+	}
+	
+	/**
+	 * Elimina un diputado de todos los grupos afines donde se encuentre.
+	 * @param nombre - Nombre del diputado a eliminar.
+	 */
+	public void removeDiputado(String nombre) {
+		for (GrupoAfinPorDiputado grup:gruposAfines.getAll()) {
+			grup.removeDiputado(nombre);
+			if (grup.esVacio()) eliminarGrupo(grup.getID());
+		}
+	}
+	
+
+	/**
+	 * Mueve un diputado de un grupo afin a otro.
+	 * @param nombreDiputado - Diputado que se debe mover.
+	 * @param desdeID - Identificador del grupo afin del que se extrae el diputado.
+	 * @param hastaID - Identificador del grupo afin al que se agrega el diputado.
+	 */
+	public void moveDiputado(String nombre, Integer desdeID, Integer hastaID) {
+		addDiputado(nombre, hastaID);
+		removeDiputado(nombre, desdeID);
+	}
+	
+	/**
+	 * Añade un nuevo grupo al conjunto de grupos afines.
+	 */
+	public void addGrupo(GrupoAfinPorDiputado nuevoGrupo) {
+		gruposAfines.add(nuevoGrupo.getID(), nuevoGrupo);
+	}
+	
+	/**
+	 * Elimina un grupo del resultado.
+	 * @param ID - Identificador del grupo del que es eliminado.
+	 */
+	public void eliminarGrupo(Integer ID) {
+		gruposAfines.remove(ID);
+	}
+	
+	/**
+	 * Comprueba si existe un grupo en concreto.
+	 * @param ID - Identificador del grupo que se desea comprobar.
+	 * @return <i>true</i> si el grupo existe en el conjunto de grupos afines.
+	 * <br>
+	 * <i>false</i> en cualquier otro caso..
+	 */
+	public Boolean existeGrupo(Integer ID) {
+		return gruposAfines.exists(ID);
+	}
+	
+	/**
+	 * Agrega un diputado a un grupo afin en concreto.
+	 * @param nombre - Nombre del diputado a agregar.
+	 * @param ID - Identificador del grupo al que es agregado.
+	 */
+	public void addDiputado(String nombre, Integer ID) {
+		gruposAfines.get(ID).addDiputado(nombre);
+	}
+	
+	/**
+	 * Elimina un diputado de un grupo afin en concreto.
+	 * @param nombre - Nombre del diputado a eliminar.
+	 * @param ID - Identificador del grupo del que es eliminado.
+	 */
+	public void removeDiputado(String nombre, Integer ID) {
+		gruposAfines.get(ID).removeDiputado(nombre);
+		if (gruposAfines.get(ID).esVacio())
+			gruposAfines.remove(ID);
+	}
+
+	/**
+	 * Suministra un nuevo conjunto con todos los grupos afines del resultado.
+	 * @return Conjunto de grupos afines.
+	 */
+	public Conjunto<GrupoAfinPorDiputado> getGruposAfines() {
+		return new Conjunto<GrupoAfinPorDiputado>(this.gruposAfines);
+	}
+	
+	/**
+	 * Suministra un conjunto de cadenas de texto con todos los nombres de los resultados.
+	 * @return Los nombres de todos los diputados separados por grupos.
+	 */
+	public Vector<Set<String>> getResultado() {
+		Vector<Set<String>> listaResultado = new Vector<Set<String>>();
+		for (GrupoAfinPorDiputado grup:gruposAfines.getAll())
+			listaResultado.add(grup.getDiputados());
+		return listaResultado;
 	}
 	
 	/**
